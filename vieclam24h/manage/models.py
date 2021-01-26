e01d3c822ef78eb63121980aa1ad34f6b9a0752d
thuan20132000 <thuan20132000@gmail.com>
2021-01-26 22:11:42 +0700

    Add AdminCategory and upload image

--- vieclam24h/manage/models.py
+++ vieclam24h/manage/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Category(models.Model):
+
+
+
+    STATUS_CHOICES = (
+        ("pending","PENDING"),
+        ("daft","DRAFT"),
+        ("published","PUBLISHED")
+    )
+
+    name = models.CharField(max_length=250)
+    slug = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='uploads/category/',blank=True)
+    status = models.CharField(max_length=10,choices=STATUS_CHOICES,default="pending")
+
+
+    def __str__(self):
+        return self.name
